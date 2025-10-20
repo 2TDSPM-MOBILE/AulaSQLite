fd4ee502fc372b8d3db34f86c60d31399c393355
Prof. Fernando Pinéo <pf1963@fiap.com.br>
2025-10-20 12:07:57 -0300

    Finalizado aula CRUD com SQLite, implementado a rota parametrizada com edit, e incluso a função para deletar nota

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { getNotes } from "@/src/db/notes";
+import { deleteNote, getNotes } from "@/src/db/notes";
 import { useFocusEffect, useRouter } from "expo-router";
 import { useCallback, useState } from "react";
 import { Button, FlatList, Text, View } from "react-native";
@@ -14,6 +14,12 @@ export default function HomeScreen() {
     }, [])
   )
 
+  //Função para deletar a note
+  function handleDelete(id:number){
+    deleteNote(id)//Deleta do banco
+    setNotes(getNotes())//Atualiza a lista
+  }
+
   return (
     <View>
       <Button title="Adicionar nota" onPress={() => router.push("/add")} />
@@ -21,8 +27,21 @@ export default function HomeScreen() {
         data={notes}
         renderItem={({ item }) => (
           <View style={{borderBottomWidth:1, padding:10,marginBottom:5}}>
-            <Text style={{fontSize:16}}>{item.title}</Text>
-            <Text>{item.content}</Text>
+            <Text style={{fontSize:16,fontWeight:'bold'}}>{item.title}</Text>
+            <Text>{item.content} - ID:{item.id}</Text>
+            <View style={{flexDirection:'row',gap:5}}>
+              {/* Botão de editar a nota*/}              
+                <Button 
+                  title="Editar" 
+                  onPress={()=>router.push(`./edit/${item.id}`)}
+                />              
+
+              {/* Botão para deletar a nota */}              
+                <Button 
+                  title="Deletar" 
+                  color='red' 
+                  onPress={()=>handleDelete(item.id)}/>              
+            </View>
           </View>
         )}
       />
